76e9381c0bdee35aa0c1c4e03d7ea17474d02de6
120356aa <yurlovandrew@gmail.com>
2022-03-15 23:57:34 -0500

    config fix

--- client/vue.config.js
+++ client/vue.config.js
@@ -1,11 +1,11 @@
 const { defineConfig } = require('@vue/cli-service')
 module.exports = defineConfig({
   transpileDependencies: true,
-  configureWebpack: {
-    proxy: {
-      '/api': {
-        target: 'http://localhost:3000',
-      }
-    }
-  }
+  // configureWebpack: {
+  //   proxy: {
+  //     '/api': {
+  //       target: 'http://localhost:3000',
+  //     }
+  //   }
+  // }
 })
--- config.js
+++ config.js
@@ -1,4 +1,4 @@
 module.exports = {
-    mongoUri: process.env.MONGO_URI || 'mongodb+srv://admin:H%40cked123@cluster0.gnvyc.mongodb.net/myFirstDatabase?retryWrites=true&w=majority',
+    mongoUri: process.env.MONGO_URI,
     PORT: process.env.PORT || 3000,
 }
